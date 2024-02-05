b037ded7e9a40fb6f583f2d80c54dcaf30484852
Seixas <carlosseixasof@gmail.com>
2024-02-05 08:48:07 -0300

    paginando a listagem de customers

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,11 +1,37 @@
 import Customer from '../typeorm/entities/Customers';
 import { CustomersRepository } from '../typeorm/repositories/CustomersRepository';
 
-const ListCustomerService = {
-    async execute(): Promise<Customer[]> {
-        const customers = await CustomersRepository.find();
+interface IPaginateCustomer {
+    from: number;
+    to: number;
+    perPage: number;
+    total: number;
+    currentPage: number;
+    prevPage: number | null;
+    nextPage: number | null;
+    data: Customer[];
+}
 
-        return customers;
+const ListCustomerService = {
+    async execute(): Promise<IPaginateCustomer> {
+        const page = 1;
+        const limit = 10;
+
+        const [result, total] = await CustomersRepository.createQueryBuilder()
+            .skip((page - 1) * limit)
+            .take(limit)
+            .getManyAndCount();
+
+        return {
+            from: (page - 1) * limit + 1,
+            to: page * limit,
+            perPage: limit,
+            total,
+            currentPage: page,
+            prevPage: page > 1 ? page - 1 : null,
+            nextPage: total > page * limit ? page + 1 : null,
+            data: result,
+        };
     },
 };
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,12 +6,14 @@ import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
 import '@shared/typeorm';
 import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(pagination);
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
